cac43adcfc33ddd2f7f110faac83e4ffbdcb9466
laricko <ttttmmm6432@gmail.com>
2025-04-22 04:26:29 -0400

    Fix logging

--- src/main.py
+++ src/main.py
@@ -3,22 +3,13 @@ import logging
 from app.utils.db import check_db_connection
 from telegram_bot.main import create_app as create_telegram_app
 
-
-class ExcludeHttpxFilter(logging.Filter):
-    def filter(self, record):
-        return not record.name.startswith("httpx")
-
-
+logging.getLogger("httpx").setLevel(logging.WARNING)
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s", level=logging.INFO
 )
 
-for handler in logging.getLogger().handlers:
-    handler.addFilter(ExcludeHttpxFilter())
-
-logging.getLogger("httpx").setLevel(logging.WARNING)
-
 logger = logging.getLogger(__name__)
+
 
 if __name__ == "__main__":
     logger.info("Starting application...")
