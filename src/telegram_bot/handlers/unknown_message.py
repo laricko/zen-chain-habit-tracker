170247d001c3d062f65270ab7fd7827fadad542c
laricko <ttttmmm6432@gmail.com>
2025-04-24 05:51:44 -0400

    Add default markp for error handling

--- src/telegram_bot/handlers/unknown_message.py
+++ src/telegram_bot/handlers/unknown_message.py
@@ -1,7 +1,11 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+from telegram_bot.consts import DEFAULT_MARKUP
+
+
 async def unknown_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
-        "❓ Sorry, I didn't understand that. Please use the menu or type a valid command."
+        "❓ Sorry, I didn't understand that. Please use the menu or type a valid command.",
+        reply_markup=DEFAULT_MARKUP
     )
